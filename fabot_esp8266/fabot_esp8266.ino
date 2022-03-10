4ea369a17d8f1ff6e1621dd0f360b5d663173905
Gonzalo Olave <growolff@gmail.com>
2022-03-10 17:04:28 -0300

    actualiza funciones, probado en la puerta del fab

--- fabot_esp8266/fabot_esp8266.ino
+++ fabot_esp8266/fabot_esp8266.ino
@@ -1,6 +1,6 @@
 /*******************************************************************
    Telegram Bot for esp8266
-   
+
 *******************************************************************/
 #include "secrets.h"
 
@@ -13,16 +13,19 @@
 X509List cert(TELEGRAM_CERTIFICATE_ROOT);
 #endif
 
+const int num_user = 3;
+const char* users[num_user] = {ID_GROW, ID_VCR, ID_JUC};
+
 // Initialize Telegram BOT
 WiFiClientSecure client;
 UniversalTelegramBot bot(BOTtoken, client);
 
 const int ledPin = 2;
-const int relePin = 5; //D1 -> GPIO5
+const int relePin = 14; //D5 -> GPIO14
 bool ledState = HIGH;
 
 // Checks for new messages every 1 second.
-int botRequestDelay = 1000;
+int botRequestDelay = 500;
 unsigned long lastTimeBotRan;
 
 void WiFi_connect() {
@@ -53,7 +56,7 @@ void setup() {
   pinMode(relePin, OUTPUT); // initialize digital ledPin as an output.
   delay(10);
   digitalWrite(ledPin, ledState); // initialize pin as off
-
+  digitalWrite(relePin, !ledState);
   delay(100);
 }
 
@@ -75,60 +78,60 @@ void loop() {
 void handleNewMessages(int numNewMessages) {
   Serial.println("handleNewMessages");
   Serial.println(String(numNewMessages));
-
-  for (int i = 0; i < numNewMessages; i++) {
+  for( int n = 0; n<num_user ; n++){
     // Chat id of the requester
-    String chat_id = String(bot.messages[i].chat_id);
-    if (chat_id != CHAT_ID) {
-      bot.sendMessage(chat_id, "Unauthorized user", "");
-      continue;
-    }
+    String chat_id = String(bot.messages[0].chat_id);
+    if (chat_id == users[n]) {
+        // Print the received message
+        String text = bot.messages[0].text;
+        Serial.println(text);
 
-    // Print the received message
-    String text = bot.messages[i].text;
-    Serial.println(text);
+        String from_name = bot.messages[0].from_name;
 
-    String from_name = bot.messages[i].from_name;
+        if (text == "/start") {
+          String welcome = "Welcome, " + from_name + ".\n";
+          welcome += "Use the following commands to control your outputs.\n\n";
+          welcome += "/led_on to turn board led ON \n";
+          welcome += "/led_off to turn board led OFF \n";
+          welcome += "/open to open the door \n";
+          welcome += "/state to request current GPIO state \n";
+          bot.sendMessage(chat_id, welcome, "");
+        }
 
-    if (text == "/start") {
-      String welcome = "Welcome, " + from_name + ".\n";
-      welcome += "Use the following commands to control your outputs.\n\n";
-      welcome += "/led_on to turn GPIO ON \n";
-      welcome += "/led_off to turn GPIO OFF \n";
-      welcome += "/state to request current GPIO state \n";
-      bot.sendMessage(chat_id, welcome, "");
-    }
+        if (text == "/led_on") {
+          bot.sendMessage(chat_id, "LED state set to ON", "");
+          ledState = LOW;
+          digitalWrite(ledPin, ledState);
+        }
 
-    if (text == "/led_on") {
-      bot.sendMessage(chat_id, "LED state set to ON", "");
-      ledState = LOW;
-      digitalWrite(ledPin, ledState);
-    }
-
-    if (text == "/led_off") {
-      bot.sendMessage(chat_id, "LED state set to OFF", "");
-      ledState = HIGH;
-      digitalWrite(ledPin, ledState);
-    }
-    if (text == "/open") {
-      bot.sendMessage(chat_id, "Open", "");
-      ledState = LOW;
-      digitalWrite(ledPin, ledState);
-      digitalWrite(relePin, HIGH);
-      delay(1000);
-      bot.sendMessage(chat_id, "Close", "");
-      ledState = HIGH;
-      digitalWrite(ledPin, ledState);
-      digitalWrite(relePin, LOW);
-    }
-
-    if (text == "/state") {
-      if (!digitalRead(ledPin)) {
-        bot.sendMessage(chat_id, "LED is ON", "");
+        if (text == "/led_off") {
+          bot.sendMessage(chat_id, "LED state set to OFF", "");
+          ledState = HIGH;
+          digitalWrite(ledPin, ledState);
+        }
+        if (text == "/open") {
+          bot.sendMessage(chat_id, "Open door", "");
+          ledState = LOW;
+          digitalWrite(ledPin, ledState);
+          digitalWrite(relePin, !ledState);
+          delay(500);
+          //bot.sendMessage(chat_id, "Close", "");
+          ledState = HIGH;
+          digitalWrite(ledPin, ledState);
+          digitalWrite(relePin, !ledState);
+        }
+        if (text == "/state") {
+          if (!digitalRead(ledPin)) {
+            bot.sendMessage(chat_id, "LED is ON", "");
+          }
+          else {
+            bot.sendMessage(chat_id, "LED is OFF", "");
+          }
+        }
       }
-      else {
-        bot.sendMessage(chat_id, "LED is OFF", "");
-      }
-    }
+  else {
+    //bot.sendMessage(chat_id, "Unauthorized user", "");
+    continue;
   }
+}
 }
